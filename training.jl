f80cb4189d1b9e49012fd3dbeaee0ce3bd0b9b90
Robert Toth <robert.toth@tangent.works>
2018-08-21 15:13:26 +0200

    added safety checks

--- training.jl
+++ training.jl
@@ -66,12 +66,20 @@ function train_hupo!()
     end
 
     if (epoch % 100 == 0)
+      println("$epoch")
       known_state = [1; 1; 4; 1; 1; 3; 5; 1; 5; 2; 5; 3; 0; 2; 0; 0; 0; 0]
       n1 = net_top_move(known_state).data
       println("Safety check moving: $(n1) which is $(n1[2]/(n1[1] + n1[2])*100) %")
       known_state = [1; 1; 1; 2; 1; 3; 5; 1; 4; 1; 5; 2; 1; 2; 1; 0; 0; 0]
       n2 = net_top_pass(known_state).data
       println("Safety check passing: $(n2) which is $(n2[4]/(n2[4] + n2[5] + n2[6])*100) %")
+      known_state = [1; 1; 4; 3; 1; 3; 5; 1; 5; 2; 5; 3; 0; 2; 0; 0; 0; 0]
+      n1 = net_top_move(known_state).data
+      println("Safety check moving: $(n1) which is $(n1[4]/(n1[1] + n1[4])*100) %")
+      known_state = [1; 1; 5; 2; 1; 3; 4; 1; 4; 2; 4; 3; 1; 2; 1; 0; 0; 0]
+      n2 = net_top_pass(known_state).data
+      println("Safety check passing: $(n2) which is $(n2[5]/(n2[4] + n2[5] + n2[6])*100) %")
+      println()
     end
 
     #check against random net
